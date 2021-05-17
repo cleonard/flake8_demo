53e7fad98303560fe20f18c23f1c43ac4b03a9e8
cleonard <ccl@chrisleonard.com>
2021-05-17 16:49:44 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# flake8 Demo
+
+- [flake8 on GitHub](https://github.com/PyCQA/flake8)
+- [flake8 docs](https://flake8.pycqa.org/en/latest/)
+
+`flake8 --select E123,W456 --enable-extensions H111`
+`# flake8: noqa`
---
+++ tests/test.py
@@ -0,0 +1,3 @@
+import os
+
+long_line = "Praesent sed sodales neque, at consequat turpis. Donec sed risus eget urna semper vulputate. Donec ullamcorper sapien vel nisl tincidunt placerat. Integer at ornare velit. Sed at elementum justo, et molestie enim. Donec ut urna eget sem convallis sodales et non mauris. "
