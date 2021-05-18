19547bb5e4e5d47e1873f933d2df77f9b025f2cd
cleonard <ccl@chrisleonard.com>
2021-05-17 20:17:17 -0500

    Added license

--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,12 @@
+"""
+----------------------------------------------------------------------------
+"THE BEER-WARE LICENSE" (Revision 42):
+<ccl@chrisleonard.com> wrote this file.  As long as you retain this notice you
+can do whatever you want with this stuff. If we meet some day, and you think
+this stuff is worth it, you can buy me a beer in return.
+----------------------------------------------------------------------------
+"""
+
 import os
 
 long_line = "Praesent sed sodales neque, at consequat turpis. Donec sed risus eget urna semper vulputate. Donec ullamcorper sapien vel nisl tincidunt placerat. Integer at ornare velit. Sed at elementum justo, et molestie enim. Donec ut urna eget sem convallis sodales et non mauris. "
